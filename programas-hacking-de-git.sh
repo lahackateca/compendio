9314dccfa9626a35abfc68947dad30123e5b803c
lahackateca <hola@lahackateca.ar>
2024-08-19 14:09:51 -0300

    Update programas-hacking-de-git.sh
    El salto de linea tenia la barra al revés, debía ser: \n

--- programas-hacking-de-git.sh
+++ programas-hacking-de-git.sh
@@ -1,26 +1,26 @@
-printf "/n-> INSTALANDO Privilege escalation awesome/n" &&
+printf "\n-> INSTALANDO Privilege escalation awesome\n" &&
 sudo git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite.git  &&
 sudo git clone https://github.com/leebaird/discover.git  &&
-printf "/n-> INSTALANDO ReconFTW/n" &&
+printf "\n-> INSTALANDO ReconFTW\n" &&
 git clone https://github.com/six2dez/reconftw &&
 cd reconftw/ && sudo chmod +x install.sh && sudo ./install.sh && cd .. &&
-printf "/n-> INSTALANDO SSH AUDIT/n" &&
+printf "\n-> INSTALANDO SSH AUDIT\n" &&
 git clone https://github.com/jtesta/ssh-audit.git &&
-printf "/n-> INSTALANDO Sublist3r/n" &&
+printf "\n-> INSTALANDO Sublist3r\n" &&
 sudo git clone https://github.com/aboul3la/Sublist3r.git  &&
-printf "/n-> INSTALANDO GitDumper/n" &&
+printf "\n-> INSTALANDO GitDumper\n" &&
 git clone https://github.com/internetwache/GitTools.git &&
-printf "/n-> INSTALANDO GitDorks/n" &&
+printf "\n-> INSTALANDO GitDorks\n" &&
 git clone https://github.com/techgaun/github-dorks && cd github-dorks && unzip master.zip && cd github-dorks-master && pip install . && cd ../.. &&
-printf "/n-> INSTALANDO LAZY RECON/n" &&
+printf "\n-> INSTALANDO LAZY RECON\n" &&
 sudo git clone https://github.com/nahamsec/bbht.git && cd bbht && sudo chmod +x install.sh && ./install.sh && cd .. &&
-printf "/n-> INSTALANDO FAST-GOOGLE-DORKS-SCAN/n" &&
+printf "\n-> INSTALANDO FAST-GOOGLE-DORKS-SCAN\n" &&
 git clone https://github.com/IvanGlinkin/Fast-Google-Dorks-Scan.git &&
 cd Fast-Google-Dorks-Scan &&
 sudo chmod 700 FGDS.sh &&
 cd .. &&
-printf "/n-> INSTALANDO SUDOMY/n" &&
+printf "\n-> INSTALANDO SUDOMY\n" &&
 sudo git clone --recursive https://github.com/screetsec/Sudomy.git &&
 cd Sudomy && npm i -g wappalyzer wscat && python3 -m pip install -r requirements.txt && cd .. &&
-printf "/n-> INSTALANDO NGINXPWNER/n" &&
+printf "\n-> INSTALANDO NGINXPWNER\n" &&
 cd /opt && sudo git clone https://github.com/stark0de/nginxpwner && cd nginxpwner && sudo chmod +x install.sh && sudo ./install.sh
