bbefe1852866cd09e7c64e19a5d6e035cfd2bc34
Patricio Carcani <hola@lahackateca.ar>
2025-11-15 20:15:44 -0300

    Añadido Kerbrute

--- programas-hacking-de-git.sh
+++ programas-hacking-de-git.sh
@@ -1,3 +1,6 @@
+printf "\n-> INSTALANDO Kerbrute\n"
+git clone https://github.com/ropnop/kerbrute.git &&
+cd kerbrute/ && sudo make all && cd ..
 printf "\n-> INSTALANDO ReconFTW\n"
 git clone https://github.com/six2dez/reconftw &&
 cd reconftw/ && sudo chmod +x install.sh && sudo ./install.sh && cd ..
@@ -18,9 +21,7 @@ printf "\n-> DESCARGANDO LINPEAS\n"
 wget https://github.com/peass-ng/PEASS-ng/releases/latest/download/linpeas.sh && chmod +x linpeas.sh
 printf "\n-> INSTALANDO FAST-GOOGLE-DORKS-SCAN\n"
 git clone https://github.com/IvanGlinkin/Fast-Google-Dorks-Scan.git &&
-cd Fast-Google-Dorks-Scan &&
-sudo chmod 700 FGDS.sh &&
-cd ..
+cd Fast-Google-Dorks-Scan && sudo chmod 700 FGDS.sh && cd ..
 printf "\n-> INSTALANDO SUDOMY\n"
 sudo git clone --recursive https://github.com/screetsec/Sudomy.git &&
 cd Sudomy && npm i -g wappalyzer wscat && python3 -m pip install -r requirements.txt && cd ..
